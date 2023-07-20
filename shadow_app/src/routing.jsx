315c5a7860b0084c11cd13855b5106d0ee657a89
whanod <whanod@gmail.com>
2023-07-20 10:21:03 +0330

    add faucet

--- shadow_app/src/routing.jsx
+++ shadow_app/src/routing.jsx
@@ -36,7 +36,7 @@ export const router = createBrowserRouter([
                     <>
                         <ArmInToken />
                         <br />
-                        <ArmOutToken />
+                        
                     </>
                 ),
             },
