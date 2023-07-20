dbcafa1ea9426a8ecd00ca07721b8be8384eab11
whanod <whanod@gmail.com>
2023-07-20 19:42:05 +0330

    Fix initpool

--- shadow_app/src/sections/liquidity/InitPool.jsx
+++ shadow_app/src/sections/liquidity/InitPool.jsx
@@ -1,80 +1,22 @@
 import { useState, useEffect, useContext } from "react";
-import {
-    Button,
-    Card,
-    Col,
-    Divider,
-    Form,
-    Input,
-    Row,
-    Result,
-    Spin,
-    Switch,
-    Space,
-} from "antd";
-import { FormGenerator } from "../../components/InputForm";
+import { Button, Card, Col, Form, Row, Result, Space } from "antd";
 import axios from "axios";
-import init, * as aleo from "@aleohq/wasm";
+import init from "@aleohq/wasm";
 import { AppContext } from "../../App";
-import { ArmOutToken } from "../faucet/ArmOutToken";
-
-
+import { armin_token,armout_token, node_url,shadow_swap } from "../../app.json";
 
 await init();
 
-export const InitPool = () => {
-    const [executionFeeRecord, setExecutionFeeRecord] = useState(null);
-    const [executeUrl, setExecuteUrl] = useState("https://vm.aleo.org/api");
-    const [functionID, setFunctionID] = useState(null);
-    const [executionFee, setExecutionFee] = useState("1");
-    const [inputs, setInputs] = useState(null);
-    const [feeLoading, setFeeLoading] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [program, setProgram] = useState(null);
+export const ArmInToken = () => {
+    let { account, fee, setFee, setArmInToken } = useContext(AppContext);
+    const program = shadow_swap.program
+    const functionID = shadow_swap.init_function;
+    const feeAmount = shadow_swap.init_fee;
+
     const [programResponse, setProgramResponse] = useState(null);
     const [executionError, setExecutionError] = useState(null);
-    const [programID, setProgramID] = useState(null);
-    const [status, setStatus] = useState("");
     const [transactionID, setTransactionID] = useState(null);
     const [worker, setWorker] = useState(null);
-    const [executeOnline, setExecuteOnline] = useState(true);
-    const [programInputs, setProgramInputs] = useState(null);
-    const [tip, setTip] = useState("Executing Program...");
-    let { account } = useContext(AppContext)
-    const [arminAmount,setArminAmount] = useState("")
-    const [armoutAmount,setArmoutAmount] = useState("")
-    const privateKey = () =>
-        account !== null ? account.to_string() : "";
-    const address = () =>
-        account !== null ? account.to_address().to_string() : "";
-    const program_string = "program armin_token.aleo;\n\nrecord ArminToken:\n    owner as address.private;\n    amount as u64.private;\n\nmapping supply_armin:\n    key left as u8.public;\n    value right as u64.public;\n\nmapping programs_armin:\n    key left as field.public;\n    value right as u64.public;\n\nfunction mint_armin:\n    input r0 as address.private;\n    input r1 as u64.private;\n    lte r1 100000u64 into r2;\n    assert.eq r2 true ;\n    cast r0 r1 into r3 as ArminToken.record;\n    output r3 as ArminToken.record;\n   finalize r1;\n\nfinalize mint_armin:\n    input r0 as u64.public;\n    get.or_use supply_armin[0u8] 0u64 into r1;\n    add r1 r0 into r2;\n    lte r2 100000000000u64 into r3;\n    assert.eq r3 true ;\n    set r2 into supply_armin[0u8];\n\nfunction transfer_armin:\n    input r0 as ArminToken.record;\n    input r1 as address.private;\n    input r2 as u64.private;\n    sub r0.amount r2 into r3;\n    cast r0.owner r3 into r4 as ArminToken.record;\n    cast r1 r2 into r5 as ArminToken.record;\n    output r4 as ArminToken.record;\n    output r5 as ArminToken.record;\n\nfunction transfer_armin_to_program:\n    input r0 as ArminToken.record;\n    input r1 as u64.private;\n    sub r0.amount r1 into r2;\n    cast r0.owner r2 into r3 as ArminToken.record;\n    output r3 as ArminToken.record;\n   finalize r1;\n\nfinalize transfer_armin_to_program:\n    input r0 as u64.public;\n    hash.bhp256 0u8 into r1 as field;\n    get.or_use programs_armin[r1] 0u64 into r2;\n    add r2 r0 into r3;\n    set r3 into programs_armin[r1];\n\nfunction transfer_armin_from_program:\n    input r0 as address.private;\n    input r1 as u64.private;\n    cast r0 r1 into r2 as ArminToken.record;\n    output r2 as ArminToken.record;\n   finalize r1;\n\nfinalize transfer_armin_from_program:\n    input r0 as u64.public;\n    hash.bhp256 0u8 into r1 as field;\n    get.or_use programs_armin[r1] 0u64 into r2;\n    sub r2 r0 into r3;\n    set r3 into programs_armin[r1];\n"
-
-    const getProgramInputs = () => {
-
-        const programManifest = [];
-        if (program) {
-            try {
-                const aleoProgram = aleo.Program.fromString(program);
-                const functions = aleoProgram.getFunctions();
-                for (let i = 0; i < functions.length; i++) {
-                    const functionManifest = { functionID: functions[i] };
-                    try {
-                        const functionInputs = aleoProgram.getFunctionInputs(
-                            functions[i],
-                        );
-                        functionManifest["inputs"] = functionInputs;
-                        programManifest.push(functionManifest);
-                    } catch (e) {
-                        console.error(e);
-                    }
-                }
-                setProgramInputs(programManifest);
-                return programManifest;
-            } catch (e) {
-                console.error(e);
-            }
-        }
-    };
 
     function spawnWorker() {
         let worker = new Worker(
@@ -82,9 +24,12 @@ export const InitPool = () => {
             { type: "module" },
         );
         worker.addEventListener("message", (ev) => {
-            if (ev.data.type == "EXECUTION_TRANSACTION_COMPLETED") {
+            if (ev.data.type == "OFFLINE_EXECUTION_COMPLETED") {
+                setTransactionID(null);
+                setExecutionError(null);
+                setProgramResponse(ev.data.outputs);
+            } else if (ev.data.type == "EXECUTION_TRANSACTION_COMPLETED") {
                 let [transaction, url] = ev.data.executeTransaction;
-                console.log(transaction)
                 axios
                     .post(
                         url + "/testnet3/transaction/broadcast",
@@ -95,29 +40,41 @@ export const InitPool = () => {
                             },
                         },
                     )
-                    .then((response) => {
-                        setFeeLoading(false);
-                        setLoading(false);
+                    .then((res) => {
                         setProgramResponse(null);
                         setExecutionError(null);
-                        setTip("Executing Program...");
-                        setTransactionID(response.data);
+                        setTransactionID(res.data);
+
+                        axios
+                            .get(`${url}/testnet3/transaction/${transactionID}`)
+                            .then((response) => {
+                                const tx = response.data;
+
+                                const encyptedFeeRecord =
+                                    tx.fee.transition.outputs[0].value;
+                                setFee(
+                                    account.to_view_key.decrypt(
+                                        encyptedFeeRecord,
+                                    ),
+                                );
+
+                                const encryptedArmInRecord =
+                                    tx.execution.transitions[0].outputs[0]
+                                        .value;
+                                const encryptedArmOutRecord =
+                                        tx.execution.transitions[0].outputs[1]
+                                            .value;
+
+                                setArmInToken(
+                                    account.to_view_key.decrypt(
+                                        encryptedArmInRecord,
+                                    ),
+                                );
+                            });
                     });
-            } else if (ev.data.type == "EXECUTION_FEE_ESTIMATION_COMPLETED") {
-                let fee = ev.data.executionFee;
-                setFeeLoading(false);
-                setLoading(false);
-                setProgramResponse(null);
-                setExecutionError(null);
-                setTransactionID(null);
-                setTip("Executing Program...");
-                setExecutionFee(fee.toString());
             } else if (ev.data.type == "ERROR") {
-                setFeeLoading(false);
-                setLoading(false);
                 setProgramResponse(null);
                 setTransactionID(null);
-                setTip("Executing Program...");
                 setExecutionError(ev.data.errorMessage);
             }
         });
@@ -141,85 +98,45 @@ export const InitPool = () => {
             };
             worker.onerror = (error) => {
                 setExecutionError(error);
-                setFeeLoading(false);
-                setLoading(false);
                 setProgramResponse(null);
                 setTransactionID(null);
-                setTip("Executing Program...");
                 reject(error);
             };
             worker.postMessage(message);
         });
     }
 
-    const execute = async () => {
-        setLoading(true);
-        setTip("Executing Program...");
-        setProgramResponse(null);
-        setTransactionID(null);
-        setExecutionError(null);
-
-        const feeAmount = parseFloat(feeString());
-
-
-        let functionInputs = [];
-        try {
-            if (inputs) {
-                functionInputs = [address(), "Armin_token",arminAmount,"armout",armoutAmount]
-            }
-        } catch (e) {
-            setExecutionError("Inputs are not valid");
-            setFeeLoading(false);
-            setLoading(false);
-            setTip("Executing Program...");
-            return;
-        }
-
-        if (executeOnline) {
-            await postMessagePromise(worker, {
-                type: "ALEO_EXECUTE_PROGRAM_ON_CHAIN",
-                remoteProgram: program_string,
-                aleoFunction: "mint_lp_init_shadow",
-                inputs: functionInputs,
-                privateKey: privateKey(),
-                fee: 1,
-                feeRecord: "{  owner: aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px.private,  microcredits: 93750000000000u64.private,  _nonce: 4584221379804783317330395146930760519734799446305091160163494666308250813255group.public}",
-                url: "http://localhost:3030",
-            });
-        }
-    };
-
-
-
-
-
-
-    const onArminChange = (event)=>{
-        if (event.target.value !== null) {
-            setArminAmount(event.target.value);
-        }
-    }
-    const onArmoutChange = (event)=>{
-        setArmoutAmount(event.target.value)
-    }
-
-
-
-    const layout = { labelCol: { span: 3 }, wrapperCol: { span: 21 } };
-    const inputsString = () => (inputs !== null ? inputs : "");
-
-    const programString = () => (program !== null ? program : "");
-    const programIDString = () => (programID !== null ? programID : "");
-    const feeRecordString = () =>
-        executionFeeRecord !== null ? executionFeeRecord : "";
     const transactionIDString = () =>
         transactionID !== null ? transactionID : "";
     const executionErrorString = () =>
         executionError !== null ? executionError : "";
     const outputString = () =>
         programResponse !== null ? programResponse.toString() : "";
-    const feeString = () => (executionFee !== null ? executionFee : "");
-    const peerUrl = () => (executeUrl !== null ? executeUrl : "");
+
+    const execute = async () => {
+        setProgramResponse(null);
+        setTransactionID(null);
+        setExecutionError(null);
+
+        let functionInputs = [
+            account.to_address().to_string(),
+            Math.floor(Math.random() * 100000) + 1 + "u64",
+        ];
+
+        await postMessagePromise(worker, {
+            type: "ALEO_EXECUTE_PROGRAM_ON_CHAIN",
+            remoteProgram: program,
+            aleoFunction: functionID,
+            inputs: functionInputs,
+            privateKey: account.to_string(),
+            fee: feeAmount,
+            feeRecord: fee,
+            url: node_url,
+        });
+    };
+
+    const layout = { labelCol: { span: 3 }, wrapperCol: { span: 21 } };
+
     return (
         <Card
             title={"Initalize Liquidity Pool"}
