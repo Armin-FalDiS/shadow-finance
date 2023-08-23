37de7a367021f40a42bbaf88009154cc929af211
whanod <whanod@gmail.com>
2023-08-23 11:02:04 +0330

    Add socialbar componet

---
+++ shadow_app/src/SocialBar.tsx
@@ -0,0 +1,32 @@
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faGithub,
+    faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+export const SocialBar = () => {
+    return (
+        <div className="social-container">
+            <a
+                href="https://www.youtube.com/c/jamesqquick"
+                className="youtube social"
+            >
+                <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </a>
+            <a
+                href="https://github.com/Armin-FalDis/shadow-finance"
+                className="github social"
+            >
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a
+                href="https://twitter.com/Shadowfi_xyz"
+                className="twitter social"
+            >
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+        </div>
+    );
+};
